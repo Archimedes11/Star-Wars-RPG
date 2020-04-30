6d1d77db74b253ea673367f68673546df7b1ca1b
Adam Wolff <adamkwolff@gmail.com>
2020-04-30 08:17:58 -0500

    added in the picture files and added achor tags to them. im loosely following a voutube video titled (how to create a game using javascript part 1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
+let player;
+
+
 function Player(characterType, health, attackPower, counterAttack){
     this.characterType = characterType;
     this.health = health;
     this.attackPower = attackPower;
     this.counterAttack = counterAttack;
 }
+
